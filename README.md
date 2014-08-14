dff69c3ef634c72db6bd93f6da5ac7199d03b00f
hash3g <hash.3g@gmail.com>
2014-08-14 13:01:53 +0300

    script to deploy travis

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 [![Travis Build Status](https://travis-ci.org/googlefonts/fontbakery-cli.svg)](https://travis-ci.org/googlefonts/fontbakery-cli)
 [![Coveralls.io Test Coverage Status](https://img.shields.io/coveralls/googlefonts/fontbakery-cli.svg)](https://coveralls.io/r/googlefonts/fontbakery-cli)
+
+### How to setup travis deployment to push gh-pages
+
+```
+$ curl -u <username> \
+  -d '{"scopes":["public_repo"],"note":"CI: <reponame>"}' \
+  https://api.github.com/authorizations
+
+# The Travis gem provides the travis command. Use the travis command
+#  to encrypt the three environment variables listed above. Replace
+#  <token> with your GitHub authentication token
+
+$ gem install travis
+$ travis login --github-token <token>
+$ travis encrypt 'GIT_NAME="Your Name" \
+    GIT_EMAIL=you@example.com GH_TOKEN=<token>' --add
+
+$ git checkout --orphan gh-pages
+$ rm -rf *
+$ rm -rf .travis.yml .gitignore
+$ git rm --cached *
+
+$ git add .
+$ git commit -m "Initial commit"
+
+$ git push -u origin gh-pages
+
+```
---
+++ tools/travis-deploy.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# coding: utf-8
+# Copyright 2013 The Font Bakery Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
+from __future__ import print_function
+import os
+import sys
+from subprocess import Popen
+
+
+if __name__ == '__main__':
+
+    if os.environ['TRAVIS_PULL_REQUEST'].lower() == 'true':
+        sys.exit(1)
+
+    if 'GH_TOKEN' not in os.environ:
+        sys.exit(1)
+
+    repo = Popen('git config remote.origin.url').stdout
+
+    deploy_branch = 'gh-pages'
+
+    Popen("git remote set-url --push origin %s" % repo)
+    Popen("git remote set-branches --add origin %s" % deploy_branch)
+    Popen('git fetch -q')
+    Popen("git config user.name '%s'" % os.environ['GIT_NAME'])
+    Popen("git config user.email '%s'" % os.environ['GIT_EMAIL'])
+    Popen('git config credential.helper "store --file=.git/credentials"')
+
+    print("https://%s:@github.com" % os.environ['GH_TOKEN'],
+          file=open('.git/credentials', 'w'))
+    Popen("git branch {0} origin/{0}" % deploy_branch)
+
+    Popen('ls | grep -v "builds" | xargs rm')
+    os.delete('.git/credentials')
