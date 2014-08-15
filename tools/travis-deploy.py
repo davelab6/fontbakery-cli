a4f567cc5a01151ffd64b442fb0196d8175653d2
Vitaly Volkov <hash.3g@gmail.com>
2014-08-15 07:35:10 +0300

    Update travis-deploy.py

--- tools/travis-deploy.py
+++ tools/travis-deploy.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     shell('git add .')
     shell('git commit -a -m "Travis deploy"')
 
-    os.remove('.git/index.lock')
+    shell('rm -rf %s' % os.path.join(os.environ['TRAVIS_BUILD_DIR'], '.git'))
 
     shell('rm -f .git/index.lock')
     shell('git push --force origin gh-pages')
