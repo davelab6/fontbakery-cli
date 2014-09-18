a697ed5ceafc1877f1e0e3c1cbf6fc52e16af003
hash3g <hash.3g@gmail.com>
2014-09-18 05:42:05 +0300

    Reformat Description files googlefonts/fontbakery-cli#413

--- bakery_cli/report/description.py
+++ bakery_cli/report/description.py
@@ -16,8 +16,12 @@
 # See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
 from __future__ import print_function
 import os.path as op
+import re
+
 from markdown import markdown
-# from bakery_cli.report.utils import build_repo_url
+from tidylib import tidy_document
+import lxml.etree as etree
+
 from bakery_cli.report import utils as report_utils
 
 
@@ -27,13 +31,31 @@ TEMPLATE_DIR = op.join(op.dirname(__file__), 'templates')
 t = lambda templatefile: op.join(TEMPLATE_DIR, templatefile)
 
 
+def reformat_contents(path):
+    doc = etree.fromstring(open(path).read(), parser=etree.HTMLParser())
+
+    for node in doc.xpath('//*'):
+        if not node.text:
+            continue
+
+        node.text = '\n'.join(re.findall(r'(.+?[.]+)', node.text.strip()))
+        print(node.text.strip())
+
+    doc = etree.tostring(doc, pretty_print=True)
+    doc, _ = tidy_document(doc, {'show-body-only': True})
+    return doc
+
+
 def generate(config, outfile='description.html'):
     source_file = op.join(config['path'], 'DESCRIPTION.en_us.html')
     if not op.exists(source_file):
         return
+    data = open(source_file).read()
+
+    print(reformat_contents(source_file), file=open(source_file, 'w'))
+
     destfile = open(op.join(config['path'], outfile), 'w')
 
-    data = open(source_file).read()
     app_version = report_utils.git_info(config)
     print(report_utils.render_template(outfile,
         app_version=app_version, data=data, current_page=outfile,
--- setup.py
+++ setup.py
@@ -81,7 +81,8 @@ setup(
         'fontaine',
         'html5lib',
         'python-magic',
-        'markdown'
+        'markdown',
+        'pytidylib'
     ],
     setup_requires=['nose', 'mock', 'coverage'],
     dependency_links=['https://github.com/behdad/fontTools/tarball/master#egg=fonttools-2.4'],
