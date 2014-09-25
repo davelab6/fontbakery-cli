18b336b2727a096098b2b97d839583be1ccd9922
hash3g <hash.3g@gmail.com>
2014-09-25 10:24:05 +0300

    Small check issues googlefonts/fontbakery#419

--- bakery_lint/tests/metadata_test.py
+++ bakery_lint/tests/metadata_test.py
@@ -34,6 +34,7 @@ SCRAPE_DATAROOT = op.join(ROOT, 'bakery_cli', 'scrapes', 'json')
 def get_test_subset_function(path):
     def function(self):
 
+        self.operator.logger.debug('SUBSET: TEST')
         if not op.exists(path):
             self.fail('%s subset does not exist' % op.basename(path))
 
