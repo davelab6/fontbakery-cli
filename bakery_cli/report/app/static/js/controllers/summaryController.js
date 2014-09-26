07572cc67815a2f66e73896f2ed045ce5d8ec929
andriyko <ahrytskiv@gmail.com>
2014-09-26 15:20:26 +0300

    Added charts

--- bakery_cli/report/app/static/js/controllers/summaryController.js
+++ bakery_cli/report/app/static/js/controllers/summaryController.js
@@ -1,4 +1,6 @@
 myApp.controller('summaryController', function($scope, $http, summaryApi, Mixins) {
+    $scope.charts = [];
+    $scope.average_chart = null;
     $scope.tests = null;
     $scope.metrics = null;
     $scope.table_sizes = null;
@@ -29,6 +31,53 @@ myApp.controller('summaryController', function($scope, $http, summaryApi, Mixins
     });
     summaryApi.getTests().then(function(response) {
         $scope.tests = response.data;
+        var chartsum = {"success": 0, "failure": 0, "fixed": 0, "error": 0};
+        angular.forEach($scope.tests, function(results, test) {
+            var success_len = results['success'].length,
+                fixed_len = results['fixed'].length,
+                failure_len = results['failure'].length,
+                error_len = results['error'].length,
+                data = google.visualization.arrayToDataTable([
+                    ['Tests', '#'],
+                    ['Success '+success_len, success_len],
+                    ['Fixed '+fixed_len, fixed_len],
+                    ['Failed '+failure_len, failure_len],
+                    ['Error '+error_len, error_len]
+                ]),
+                options = {
+                    title: test,
+                    is3D: true,
+                    colors: ['#468847', '#3a87ad', '#b94a48', '#c09853']
+                };
+            $scope.charts.push({data: data, options: options, type: "PieChart", displayed: true});
+            chartsum = {
+                "success": chartsum.success + success_len,
+                "error": error_len,
+                "failure": chartsum.failure + failure_len,
+                "fixed": chartsum.fixed + fixed_len
+            }
+        });
+        if (Object.keys($scope.tests).length > 1) {
+            var success_len = chartsum.success,
+                fixed_len = chartsum.fixed,
+                failure_len = chartsum.failure,
+                error_len = chartsum.error,
+                data = google.visualization.arrayToDataTable([
+                    ['Tests', '#'],
+                    ['Success '+success_len, success_len],
+                    ['Fixed '+fixed_len, fixed_len],
+                    ['Failed '+failure_len, failure_len],
+                    ['Error '+error_len, error_len]
+                ]),
+                options = {
+                    title: "Average",
+                    is3D: true,
+                    colors: ['#468847', '#3a87ad', '#b94a48', '#c09853']
+                };
+            $scope.average_chart = {data: data, options: options, type: "PieChart", displayed: true};
+        }
+
+
         $scope.blockers = {};
         var watch_list = ['error', 'failure', 'fixed'];
         var watch_tag = 'required';
