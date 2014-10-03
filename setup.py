3f8ba408a382956b4783a8f7f9f8a47472a49a2a
Dave Crossland <dave@lab6.com>
2014-10-03 18:30:16 +0100

    Update setup.py with semver style version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name="FontBakery-cli",
-    version='0.1',
+    version='0.0.2',
     url='https://github.com/googlefonts/fontbakery-cli/',
     description='fontbakery-cli',
     author='Vitaliy Volkov',
