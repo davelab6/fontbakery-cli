bbb5c52f20b3bed6b61411bb505d8e38fb124218
Dave Crossland <dave@lab6.com>
2014-10-08 18:16:05 +0530

    Improve test_contour_count
    - add license
    - improve doctoring
    - make fail msg code more simple

--- bakery_lint/tests/upstream/test_contour_count.py
+++ bakery_lint/tests/upstream/test_contour_count.py
@@ -1,3 +1,19 @@
+# coding: utf-8
+# Copyright 2013 The Font Bakery Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
 import robofab.world
 
 from bakery_lint.base import BakeryTestCase
@@ -10,7 +26,7 @@ class TestContourCount(BakeryTestCase):
     name = __name__
 
     def test_contour_count(self):
-        """ In font there shouldn't be too many overlapping contours """
+        """ UFO has no overlapping contours """
         font = robofab.world.OpenFont(self.operator.path)
 
         for glyphname in font.keys():
@@ -21,5 +37,5 @@ class TestContourCount(BakeryTestCase):
             test = glyph.copy()
             test.removeOverlap()
             if count - len(test) > 2:
-                msg = "{} has a unusally high number of overlapping contours."
-                self.fail(msg.format(glyph.name))
+                msg = glyphname + " has overlapping contours."
+                self.fail(msg)
