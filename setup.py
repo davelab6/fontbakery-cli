a57b706ef5011fb747ba4a4c9522ae526bf4b948
Dave Crossland <dave@lab6.com>
2014-10-08 20:53:01 +0530

    setup.py: Add comments, improve error messages

--- setup.py
+++ setup.py
@@ -14,10 +14,56 @@
 # limitations under the License.
 #
 # See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
+import os
 
+# require python fontforge module
+msg = 'Python module `fontforge` is required. ' +
+      'Install it with `apt-get install python-fontforge` or '+ 
+      '`brew install python; brew install fontforge --HEAD`'
+try:
+    import fontforge
+except ImportError:
+    raise Exception(msg)
+
+# require ttfautohint
+msg = 'Command line tool `ttfautohint` is required. ' +
+    'Install it with `apt-get install ttfautohint` or ' +
+    '`brew install ttfautohint`'
+assert [os.path.join(p, 'ttfautohint')
+        for p in os.environ.get('PATH').split(':')
+        if os.path.exists(os.path.join(p, 'ttfautohint'))], msg
+
+# require libmagic
+import ctypes
+import ctypes.util
+libmagic = None
+# Let's try to find magic or magic1
+dll = ctypes.util.find_library('magic') or ctypes.util.find_library('magic1')
+
+# This is necessary because find_library returns None if it doesn't find the library
+if dll:
+    libmagic = ctypes.CDLL(dll)
+
+if not libmagic or not libmagic._name:
+    import sys
+    platform_to_lib = {'darwin': ['/opt/local/lib/libmagic.dylib',
+                                  '/usr/local/lib/libmagic.dylib'] +
+                       # Assumes there will only be one version installed
+                       glob.glob('/usr/local/Cellar/libmagic/*/lib/libmagic.dylib'),
+                       'win32':  ['magic1.dll']}
+    for dll in platform_to_lib.get(sys.platform, []):
+        try:
+            libmagic = ctypes.CDLL(dll)
+            break
+        except OSError:
+            pass
+
+if not libmagic or not libmagic._name:
+    # It is better to raise an ImportError since we are importing magic module
+    raise ImportError('failed to find libmagic.  Check your installation')
+
+# now installation can begin!
 from setuptools import setup
-
-
 setup(
     name="fontbakerycli",
     version='0.0.3',
